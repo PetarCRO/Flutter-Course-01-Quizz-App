54f6a8101ff98ece59df4c92968ffe30f46af5f5
PetarCRO <petar.prenc@gmail.com>
2022-09-11 09:58:24 +0200

    added question.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(QuizzApp());
 
-class QuizzApp extends StatelessWidget {
+class QuizzApp extends StatefulWidget {
+  @override
+  State<QuizzApp> createState() => _QuizzAppState();
+}
+
+class _QuizzAppState extends State<QuizzApp> {
+  var _questions = ['Question 1', 'Question 2', 'Question 3', 'Question 4'];
+  int _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      if (_questionIndex < _questions.length - 1)
+        _questionIndex++;
+      else
+        _questionIndex = 0;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = ['Question 1', 'Question 2', 'Question 3'];
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -15,30 +32,30 @@ class QuizzApp extends StatelessWidget {
           backgroundColor: Colors.blueGrey,
         ),
         body: Column(children: [
-          Text('this is a question'),
+          Question(_questions[_questionIndex]),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: _answerQuestion,
             child: Text('Answer A'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.blueGrey,
             ),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: _answerQuestion,
             child: Text('Answer B'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.blueGrey,
             ),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: _answerQuestion,
             child: Text('Answer C'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.blueGrey,
             ),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: _answerQuestion,
             child: Text('Answer D'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.blueGrey,
